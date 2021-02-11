aeb3fdde60038906f5edc2741d14d3d414746ddd
Jan Redmann <janredmann1@gmail.com>
2021-02-11 12:44:59 +0100

    add axios/ajax-get

---
+++ ajax-get/index.js
@@ -0,0 +1,22 @@
+const getDataForCountry = country => {
+    // fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+    //     .then(response => response.json())
+    //     .then(data => console.log(data))
+    axios.get(`https://restcountries.eu/rest/v2/name/${country}`)
+        .then(response => {
+            const countryDetail = response.data[0]
+            // set the country name
+            document.querySelector('#country-name').innerText = countryDetail.name;
+            // set population
+            document.querySelector('#country-population').innerText = countryDetail.population;
+
+            document.querySelector('#country-flag')
+                .setAttribute('src', countryDetail.flag)
+        })
+}
+
+document.querySelector('button').onclick = () => {
+    const userInput = document.querySelector('#name').value;
+    console.log(userInput);
+    getDataForCountry(userInput)
+}
